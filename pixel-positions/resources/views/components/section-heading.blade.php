7fc5e4c30a8500af947649c528fe0ca639db2854
halloween-jack <halloween.jack.dev@gmail.com>
2024-05-30 19:30:37 +0900

    learned episode27

---
+++ pixel-positions/resources/views/components/section-heading.blade.php
@@ -0,0 +1,7 @@
+<div class="inline-flex gap-x-2 items-center">
+    <span class="inline-block w-2 h-2 bg-white"></span>
+
+    <h3 class="text-lg font-bold">
+        {{ $slot }}
+    </h3>
+</div>
---
+++ pixel-positions/resources/views/components/tag.blade.php
@@ -0,0 +1,2 @@
+<a href="#"
+    class="py-1 px-2 text-xs rounded-xl transition-colors duration-300 bg-white/10 hover:bg-white/25">{{ $slot }}</a>
